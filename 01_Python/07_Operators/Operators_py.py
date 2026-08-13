3ae2b20c4e395e8845e89c5778dbb7cfeeab6cc6
Zeeshan Jamshed <zeeshanjamshed0786@gmail.com>
2026-08-13 14:35:59 +0530

    Add files via upload

---
+++ 01_Python/07_Operators/Operators_py.py
@@ -0,0 +1,71 @@
+# Python Operators - Practice
+
+# 1. Arithmetic Operators
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+
+# 2. Assignment Operators
+
+a = 10
+
+a += 5
+print(a)
+
+a -= 5
+print(a)
+
+a *= 5
+print(a)
+
+a /= 5
+print(a)
+
+a //= 2
+print(a)
+
+a %= 3
+print(a)
+
+a **= 2
+print(a)
+
+
+# 3. Relational Operators
+
+a = 10
+b = 5
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+# 4. Logical Operators
+
+a = 10
+b = 5
+
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not(a > 5))
+
+
+# 5. Unary Operators
+
+a = 10
+
+print(+a)
+print(-a)
